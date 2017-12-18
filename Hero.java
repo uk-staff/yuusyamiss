343f5a6e955178324a105598853f25a4e61e7838
shuuwakobayashi <shuuwa@d218.uk2.net.it-chiba.ac.jp>
2017-12-18 17:13:17 +0900

    Initial Commit

---
+++ Hero.java
@@ -0,0 +1,14 @@
+public class Hero {
+    String nam;
+    int hp ;
+    Sword sword;
+    void attack() {
+        System.out.println(this.name + "は攻撃した!");
+        System.out.println("敵に5ポイントのダメージを与えた");
+    }
+    Hero(String name){
+        this.hp =100;
+        this.name= name;
+    }
+}
+
